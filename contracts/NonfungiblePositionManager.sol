5cc2356a2529126032a7ae6767d98a2839ff7043
saito <bluewhale.saito@gmail.com>
2026-01-14 13:41:14 +0900

    Test Info

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -72,7 +72,8 @@ contract NonfungiblePositionManager is
         address _factory,
         address _WETH9,
         address _tokenDescriptor_
-    ) ERC721Permit('Uniswap V3 Positions NFT-V1', 'UNI-V3-POS', '1') PeripheryImmutableState(_factory, _WETH9) {
+    // ) ERC721Permit('AquaBank V3 Positions NFT-V1', 'AQUA-V3-POS', '1') PeripheryImmutableState(_factory, _WETH9) {
+    ) ERC721Permit('AquaTest V3 Positions NFT-V1', 'TEST-A-POS', '1') PeripheryImmutableState(_factory, _WETH9) {
         _tokenDescriptor = _tokenDescriptor_;
     }
 
